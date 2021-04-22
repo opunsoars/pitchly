237daf7135a6e26b41e9d272657a3cee157c5697
Vinay@Workbench <vinay.warrier@gmail.com>
2021-04-22 19:39:47 +0200

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='pitchly',
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='A python package that is a wrapper for Plotly to generate football tracking and event data plots',
     long_description='%s\n%s' % (
